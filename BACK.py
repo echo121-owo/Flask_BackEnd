aaf9a14e30dec1c66a9cbebdb7cc73fe7f673cf4
Null0981 <liyuanwang093@gmail.com>
2025-11-10 14:20:59 +1100

    Initial commit

---
+++ BACK.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app) #允许cross port
+@app.route('/')  # 访问根目录的时候调用root
+
+
+def home():
+    return jsonify({"message":"hahahah"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  #启动一个服务，监听http请求
